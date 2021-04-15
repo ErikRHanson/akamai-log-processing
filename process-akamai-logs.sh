0b785904eb46bd90a4fb1ee61b3a92d92c835a89
Erik Hanson <erik.hanson@technicolor.com>
2021-04-15 09:55:13 -0600

    add s3 mv to archive logs to tap-akamai-logs bucket

--- process-akamai-logs.sh
+++ process-akamai-logs.sh
@@ -12,6 +12,10 @@
         sudo mv /data/akamai-logs/* /data/akamai-logs-unzipped/.
         sudo gunzip /data/akamai-logs-unzipped/*.gz
 
+        echo "using aws s3 mv"
+        echo "to archive Akamai logs in S3"
+        sudo aws s3 mv /data/akamai-logs-archive/ s3://tap-akamai-logs/ --recursive
+
         echo "Process complete, it's up to logstash now."
         exit 0
 }
